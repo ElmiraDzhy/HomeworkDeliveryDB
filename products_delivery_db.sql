8d5f7ddadf732a7ea217a603b39ea0311464d13b
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-08 15:06:45 +0300

    Add tables with relations

---
+++ products_delivery_db.sql
@@ -0,0 +1,48 @@
+CREATE TABLE products
+(
+    id    serial PRIMARY KEY,
+    name  varchar(300) NOT NULL CHECK ( name != '' ),
+    price numeric      NOT NULL CHECK ( price > 0 )
+);
+
+CREATE TABLE clients
+(
+    id           serial PRIMARY KEY,
+    name         varchar(300) NOT NULL CHECK ( name != '' ),
+    address      varchar(300) NOT NULL CHECK ( address != '' ),
+    phone_number varchar(20)  NOT NULL
+);
+
+CREATE TABLE invoices
+(
+    id         serial PRIMARY KEY,
+    client_id  int REFERENCES clients (id),
+    created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE orders
+(
+    id         serial PRIMARY KEY,
+    product_id int REFERENCES products (id),
+    invoice_id int REFERENCES invoices (id) UNIQUE,
+    quantity   int NOT NULL DEFAULT 1
+);
+
+CREATE TABLE shipments
+(
+    id         serial PRIMARY KEY,
+    created_at timestamp NOT NULL DEFAULT current_timestamp
+
+);
+
+ALTER TABLE invoices
+    ADD COLUMN order_id int REFERENCES orders (id); -- for 1:1 relation
+
+
+CREATE TABLE orders_to_shipments
+(
+    order_id              int REFERENCES orders (id),
+    shipment_id           int REFERENCES shipments (id),
+    quantity_for_shipment int NOT NULL,
+    PRIMARY KEY (order_id, shipment_id)
+);
